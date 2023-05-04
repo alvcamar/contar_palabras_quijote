a4be3a6225d306648901e4f61851ea864b221a6f
alvcamar <124206495+alvcamar@users.noreply.github.com>
2023-05-04 09:45:35 +0200

    Create contar_palabras.py

---
+++ contar_palabras.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: alvarocamarafernandez
+"""
+
+from pyspark import SparkContext
+import sys
+import string
+
+def escribeFichero(filename, txt):
+    with open(filename, "w") as f:
+        f.write(txt)
+    print("Información guardada en el fichero " + filename + " correctamente.")
+
+def word_split(line):
+    for c in string.punctuation+"¿!«»":
+        line = line.replace(c,' ')
+        line = line.lower()
+    return len(line.split())
+
+def main(filename):
+    with SparkContext() as sc:
+        sc.setLogLevel("ERROR")
+        lineas = sc.textFile(filename)
+        words_rdd = lineas.map(word_split)
+        print (words_rdd.collect())
+        suma = words_rdd.sum()
+        print ("El valor de la suma es: " + str(suma))
+        if filename == "quijote_s05.txt":
+            escribeFichero("out_quijote_s05.txt", str(suma))
+        else:
+            escribeFichero("out_quijote.txt", str(suma))
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Uso: python3 {0} <file>".format(sys.argv[0]))
+    else:
+        main(sys.argv[1])
